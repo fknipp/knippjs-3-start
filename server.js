7116394b7bc297c7317740f2ca7aebd5a3b47c31
fknipp <franz@qnipp.com>
2026-04-20 21:04:14 +0200

    Add generation of random number as server function

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from "express";
 import ViteExpress from "vite-express";
-import { createServer } from "vite";
 
 const app = express();
 const port = 5173;
 
 /* Add your server routes here */
+
+app.get("/random", (req, res) => {
+  console.log("Random number will be generated.");
+  const number = Math.floor(Math.random() * 100 + 1);
+  console.log(`The generated number is ${number}.`)
+  res.send({number});
+});
 
 ViteExpress.listen(app, port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
--- src/NumberGuessingGame.jsx
+++ src/NumberGuessingGame.jsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import NumberInput from "./components/NumberInput.jsx";
 import Result from "./components/Result.jsx";
 
 function NumberGuessingGame() {
-  const [randomNumber, setRandomNumber] = useState(
-    Math.floor(Math.random() * 100 + 1)
-  );
+  const [randomNumber, setRandomNumber] = useState();
+  const [loading, setLoading] = useState(true);
   const [number, setNumber] = useState(50);
   const [text, setText] = useState("Geben Sie eine Zahl von 1 bis 100 ein.");
+
+  async function fetchRandom() {
+    setLoading(true);
+    const res = await fetch("/random");
+    const data = await res.json();
+    setRandomNumber(data.number);
+    setLoading(false);
+  }
+
+  useEffect(() => { fetchRandom() }, []);
 
   const handleClick = (e) => {
     if (number > randomNumber) {
